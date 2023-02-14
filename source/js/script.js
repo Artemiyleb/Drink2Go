ce178abd3a1cb047b301d5f40b360a5ecfe69bcf
Artemiyleb <artem.lebedev.msk@gmail.com>
2023-02-15 00:19:42 +0300

    Сверстаны секции каталога и карты
    Сверстаны секции каталога и карты. Начата секция Footer

--- source/js/script.js
+++ source/js/script.js
@@ -61,3 +61,146 @@ const swiper = new Swiper('.swiper', {
     //   },
     // }
 });
+
+// Select
+
+const getTemplate = (data = [], placeholder, selectedId) => {
+    const items = data.map(item => {
+        let cls = ''
+        if (item.id === selectedId) {
+            text = item.value
+            cls = 'selected'
+        }
+        return `
+          <li class="select__item ${cls}" data-type="item" data-id="${item.id}">${item.value}</li>
+      `
+    })
+    return `
+      <input type="hidden" class="hidden__input">
+      <div class="select__backdrop" data-type="backdrop"></div>
+      <div class="select__input" data-type="input">
+          <span data-type="value">${text}</span>
+          <img src="./../img/arrow.svg" alt="arrow" data-type="arrow" class="select__arrow">
+      </div>
+      <div class="select__dropdown">
+          <ul class="select__list">
+              ${items.join('')}
+          </ul>
+      </div>
+  `
+}
+class Select {
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector)
+        this.options = options
+        this.selectedId = options.selectedId
+
+        this.render()
+        this.setup()
+    }
+
+    render() {
+        const { placeholder, data } = this.options
+        this.$el.classList.add('select')
+        this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId)
+    }
+    setup() {
+        this.clickHandler = this.clickHandler.bind(this)
+        this.$el.addEventListener('click', this.clickHandler)
+        this.$arrow = this.$el.querySelector('[data-type="arrow"]')
+        this.$value = this.$el.querySelector('[data-type="value"]')
+    }
+
+    clickHandler(event) {
+        const { type } = event.target.dataset
+        if (type === 'input') {
+            this.toggle()
+        } else if (type === 'item') {
+            const id = event.target.dataset.id
+            this.select(id)
+        } else if (type === 'backdrop') {
+            this.close()
+        }
+    }
+
+    get isOpen() {
+        return this.$el.classList.contains('open')
+    }
+
+    get current() {
+        return this.options.data.find(item => item.id === this.selectedId)
+    }
+
+    select(id) {
+        this.selectedId = id
+        this.$value.textContent = this.current.value
+
+        this.$el.querySelectorAll(`[data-type="item"]`).forEach(el => el.classList.remove('selected'))
+        this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected')
+
+        this.options.onSelect ? this.options.onSelect(this.current) : null
+        this.close()
+    }
+
+    toggle() {
+        this.isOpen ? this.close() : this.open()
+    }
+
+    open() {
+        this.$el.classList.add('open')
+        this.$arrow.classList.add('open')
+    }
+
+    close() {
+        this.$el.classList.remove('open')
+        this.$arrow.classList.remove('open')
+    }
+
+    destroy() {
+        this.$el.removeEventListener('click', this.clickHandler)
+        this.$el.innerHTML = ''
+    }
+}
+
+
+// Инициализация плагина
+const select = new Select('#select', {
+    placeholder: 'Выберите элемент',
+    selectedId: '1',
+    data: [
+        { id: '1', value: 'по умолчанию' },
+        { id: '2', value: 'сначала дорогие' },
+        { id: '3', value: 'сначала дешёвые' },
+        { id: '4', value: 'высокий рейтинг' },
+    ],
+    onSelect(item) {
+        const input = document.querySelector('.hidden__input')
+        input.value = item.value
+    }
+})
+
+// MAP
+const map = L.map('map').setView([
+    59.96840,
+    30.31748
+], 17.5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+const pinIcon = L.icon({
+    iconUrl: './../img/map-mark.svg',
+    iconSize: [38, 50],
+    iconAnchor: [38, 50],
+});
+
+const marker = L.marker({
+    lat: 59.96831,
+    lng: 30.31748,
+}, {
+    icon: pinIcon,
+});
+
+marker.addTo(map);
